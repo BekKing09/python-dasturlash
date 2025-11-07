16cf13ccdc4b0b9087db039249a82267397e68b5
BekKing <satimovali91@gmail.con>
2025-11-07 09:19:46 +0300

    bir xil sonlarni o'zgartiradi

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-# harflarni listda chiqaradigan dastur
+# # harflarni listda chiqaradigan dastur
 
-ism = input("ismingizni kiriting:>")
-letters = []
+# ism = input("ismingizni kiriting:>")
+# letters = []
 
-for i in ism:
-    letters.append(i)
+# for i in ism:
+#     letters.append(i)
 
-print("natija", letters)
+# print("natija", letters)
+
+
+
+
+
+# listdagi oxshash elementlarni o'chiriladi
+letters = [1, 2, 2, 3, 4, 4, 5]
+
+new_list = []
+
+for i in letters:
+    if i not in new_list:
+        new_list.append(i)
+print("natija", new_list)
