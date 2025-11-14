ab85bfda44f57fe1fc4868e4ad1af20269cbeafb
Behruzbek Abdurahimov <satimovali91@gmail.com>
2025-11-14 10:32:49 +0500

    topshiriq javoblari

---
+++ topshiriqlar.py
@@ -0,0 +1,72 @@
+# # 1-topshiriq
+# juftlist = []
+# toqlist = []
+
+# for i in range(1, 21):
+#     if i % 2 == 0:
+#         juftlist.append(i)
+#     elif i % 2 == 1:
+#         toqlist.append(i)
+# print("juft sonlar", juftlist)
+# print("toq sonlar", toqlist)
+
+
+
+# # 2-topshiriq
+# for i in range(1, 31):
+#     if i % 3 == 0 and i % 5 == 0:
+#         print("python dasturlash", i)
+#     elif i % 3 == 0:
+#         print("python", i)
+#     elif i % 5 == 0:
+#         print("dasturlash", i)
+#     else:
+#         print(i)
+
+
+
+
+
+# # 3-topshiriq
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # 5-topshiriq
+# lst = []
+
+
+# lst.append(ism)
+
+
+
+
+son = []
+kvadrati = []
+
+for i in range(1, 11):
+    if i:
+        son.append(i)
+        print(son)
+    elif i ** 2:
+        kvadrati.append(i)
+        print("kvadrati",kvadrati)
+
+
+
+
